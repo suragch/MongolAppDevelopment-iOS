7a261f9d09fead7118e730ccbf2ba478e6fdaf82
Suragch <suragch@icloud.com>
2015-09-30 19:50:03 +0800

    Test 1

--- Mongol App Componants/MainViewController.swift
+++ Mongol App Componants/MainViewController.swift
@@ -38,14 +38,7 @@ class MainViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    // Test 1
+    
 
 }
