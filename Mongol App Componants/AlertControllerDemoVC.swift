5fea322166e048fb004de703f45bc85287565005
Suragch <suragch@icloud.com>
2016-07-07 07:57:16 -0700

    Alert controller improvements

--- Mongol App Componants/AlertControllerDemoVC.swift
+++ Mongol App Componants/AlertControllerDemoVC.swift
@@ -16,15 +16,38 @@ class AlertControllerDemoVC: UIViewController {
         
     }
 
-    
-    @IBAction func showMongolAlertButtonTapped(sender: UIButton) {
-        
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let myAlert = storyboard.instantiateViewControllerWithIdentifier("MongolAlertControllerID")
-        myAlert.modalPresentationStyle = UIModalPresentationStyle.OverCurrentContext
-        myAlert.modalTransitionStyle = UIModalTransitionStyle.CrossDissolve
-        self.presentViewController(myAlert, animated: true, completion: nil)
+    @IBAction func showButtonlessAlertTapped(sender: UIButton) {
+        let title = "ᠳᠤᠰ ᠪᠢᠴᠢᠯᠭᠡ ᠬᠠᠭᠤᠯᠠᠭᠳᠠᠪᠠ"
+        let message = "ᠳᠤᠰ ᠪᠢᠴᠢᠯᠭᠡ ᠶᠢᠨ ᠶᠦᠨᠢᠺᠤᠳ᠋ ᠲᠡᠺᠧᠰᠲ ᠢ ᠭᠠᠷ ᠤᠳᠠᠰᠤᠨ ᠰᠢᠰᠲ᠋ᠧᠮ ᠦᠨ ᠨᠠᠭᠠᠬᠤ ᠰᠠᠮᠪᠠᠷ᠎ᠠ ᠳᠤ ᠬᠠᠭᠤᠯᠠᠭᠳᠠᠪᠠ᠃ ᠳᠠ ᠡᠭᠦᠨ ᠢ ᠪᠤᠰᠤᠳ APP ᠳᠤ ᠨᠠᠭᠠᠵᠤ ᠬᠡᠷᠡᠭᠯᠡᠵᠤ ᠪᠤᠯᠤᠨ᠎ᠠ᠃ ᠭᠡᠪᠡᠴᠦ ᠮᠤᠩᠭᠤᠯ ᠬᠡᠯᠡᠨ ᠦ ᠶᠦᠨᠢᠺᠤᠳ᠋ ᠤᠨ ᠪᠠᠷᠢᠮᠵᠢᠶ᠎ᠠ ᠨᠢᠭᠡᠳᠦᠭᠡᠳᠦᠢ ᠳᠤᠯᠠ ᠵᠠᠷᠢᠮ ᠰᠤᠹᠲ ᠪᠤᠷᠤᠭᠤ ᠦᠰᠦᠭ ᠢᠶᠡᠷ ᠢᠯᠡᠷᠡᠬᠦ ᠮᠠᠭᠠᠳ᠃ "
+        showZeroButtonAlert(withTitle: title, message: message, alertWidth: 250)
     }
+    @IBAction func showOneButtonAlertTapped(sender: UIButton) {
+        
+        let title = "ᠳᠤᠰ ᠪᠢᠴᠢᠯᠭᠡ ᠬᠠᠭᠤᠯᠠᠭᠳᠠᠪᠠ"
+        let message = "ᠳᠤᠰ ᠪᠢᠴᠢᠯᠭᠡ ᠶᠢᠨ ᠶᠦᠨᠢᠺᠤᠳ᠋ ᠲᠡᠺᠧᠰᠲ ᠢ ᠭᠠᠷ ᠤᠳᠠᠰᠤᠨ ᠰᠢᠰᠲ᠋ᠧᠮ ᠦᠨ ᠨᠠᠭᠠᠬᠤ ᠰᠠᠮᠪᠠᠷ᠎ᠠ ᠳᠤ ᠬᠠᠭᠤᠯᠠᠭᠳᠠᠪᠠ᠃ ᠳᠠ ᠡᠭᠦᠨ ᠢ ᠪᠤᠰᠤᠳ APP ᠳᠤ ᠨᠠᠭᠠᠵᠤ ᠬᠡᠷᠡᠭᠯᠡᠵᠤ ᠪᠤᠯᠤᠨ᠎ᠠ᠃ ᠭᠡᠪᠡᠴᠦ ᠮᠤᠩᠭᠤᠯ ᠬᠡᠯᠡᠨ ᠦ ᠶᠦᠨᠢᠺᠤᠳ᠋ ᠤᠨ ᠪᠠᠷᠢᠮᠵᠢᠶ᠎ᠠ ᠨᠢᠭᠡᠳᠦᠭᠡᠳᠦᠢ ᠳᠤᠯᠠ ᠵᠠᠷᠢᠮ ᠰᠤᠹᠲ ᠪᠤᠷᠤᠭᠤ ᠦᠰᠦᠭ ᠢᠶᠡᠷ ᠢᠯᠡᠷᠡᠬᠦ ᠮᠠᠭᠠᠳ᠃ "
+        let action = {
+            () -> Void in
+            print("button works")
+        }
+        showOneButtonAlert(withTitle: title, message: message, topButtonText: "ᠮᠡᠳᠡᠯ᠎ᠡ", action: action, alertWidth: 300)
+    }
+    @IBAction func showTwoButtonAlertTapped(sender: UIButton) {
+        
+        let title = "ᠤᠰᠠᠳᠬᠠᠬᠤ"
+        let message = "ᠲᠠ ᠦᠨᠡᠭᠡᠷ ᠪᠦᠬᠦ ᠵᠠᠬᠢᠵ᠎ᠠ ᠶᠢ ᠤᠰᠠᠳᠬᠠᠬᠤ ᠤᠤ?"
+        
+        let actionOne = {
+            () -> Void in
+            print("top button works")
+        }
+        let actionTwo = {
+            () -> Void in
+            print("bottom button works")
+        }
+        
+        showAlert(withTitle: title, message: message, numberOfButtons: 2, topButtonText: "ᠲᠡᠭᠡ", topButtonAction: actionOne, bottomButtonText: "ᠪᠤᠯᠢ", bottomButtonAction: actionTwo, alertWidth: 170)
+    }
+
 
     @IBAction func showStandardAlertButtonTapped(sender: UIButton) {
         
@@ -38,30 +61,38 @@ class AlertControllerDemoVC: UIViewController {
         self.presentViewController(alert, animated: true, completion: nil)
     }
     
-    @IBAction func alert2button(sender: UIButton) {
+
+    func showZeroButtonAlert(withTitle title: String?, message: String?, alertWidth: CGFloat?) {
         
-        showAlert(withTitle: "Hello", message: "This is my message", topButtonText: "OK", bottomButtonText: "Cancel")
+        //showAlert(withTitle: title, message: message, numberOfButtons: 0, topButtonText: nil, bottomButtonText: nil, alertWidth: alertSize)
+        showAlert(withTitle: title, message: message, numberOfButtons: 0, topButtonText: nil, topButtonAction: nil, bottomButtonText: nil, bottomButtonAction: nil, alertWidth: 170)
+
     }
     
-    func showAlert(withTitle title: String?, message: String?, topButtonText: String?, bottomButtonText: String?) {
+    func showOneButtonAlert(withTitle title: String?, message: String?, topButtonText: String?, action: (()->Void)?, alertWidth: CGFloat?){
+        
+        //showAlert(withTitle: title, message: message, numberOfButtons: 1, topButtonText: topButtonText, bottomButtonText: nil, alertWidth: alertSize)
+        showAlert(withTitle: title, message: message, numberOfButtons: 1, topButtonText: topButtonText, topButtonAction: action, bottomButtonText: nil, bottomButtonAction: nil, alertWidth: alertWidth)
+
+    }
+    
+    
+    func showAlert(withTitle title: String?, message: String?, numberOfButtons: Int, topButtonText: String?, topButtonAction: (()->Void)?, bottomButtonText: String?, bottomButtonAction: (()->Void)?, alertWidth: CGFloat?) {
         
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let myAlertController = storyboard.instantiateViewControllerWithIdentifier("MongolAlertController2ID") as! UIMongolAlertController2
+        let myAlertController = storyboard.instantiateViewControllerWithIdentifier("MongolAlertControllerID") as! UIMongolAlertController
         myAlertController.modalPresentationStyle = UIModalPresentationStyle.OverCurrentContext
         myAlertController.modalTransitionStyle = UIModalTransitionStyle.CrossDissolve
         
+        
         myAlertController.titleText = title
         myAlertController.messageText = message
+        myAlertController.numberOfButtons = numberOfButtons
+        myAlertController.alertWidth = alertWidth
         myAlertController.buttonOneText = topButtonText
         myAlertController.buttonTwoText = bottomButtonText
-        myAlertController.buttonOneAction = {
-            () -> Void in
-            print("botton one works")
-        }
-        myAlertController.buttonTwoAction = {
-            () -> Void in
-            print("button two works")
-        }
+        myAlertController.buttonOneAction = topButtonAction
+        myAlertController.buttonTwoAction = bottomButtonAction
         
         self.presentViewController(myAlertController, animated: true, completion: nil)
     }
--- Mongol App Componants/UIMongolAlertController.swift
+++ Mongol App Componants/UIMongolAlertController.swift
@@ -2,29 +2,144 @@
 import UIKit
 
 class UIMongolAlertController: UIViewController, UIGestureRecognizerDelegate  {
-
+    
+    // TODO: Create this view controller entirely programmatically
     @IBOutlet weak var alertTitle: UIMongolLabel!
     @IBOutlet weak var alertMessage: UIMongolLabel!
     @IBOutlet weak var alertContainerView: UIView!
+    @IBOutlet weak var topButton: UIMongolLabel!
+    @IBOutlet weak var bottomButton: UIMongolLabel!
+    
+    @IBOutlet weak var widthConstraint: NSLayoutConstraint!
+    @IBOutlet weak var buttonContainerWidthConstraint: NSLayoutConstraint!
+    @IBOutlet weak var horizontalDividerWidthConstraint: NSLayoutConstraint!
+    @IBOutlet weak var verticalDividerHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var buttonEqualHeightsConstraint: NSLayoutConstraint!
+    @IBOutlet weak var bottomButtonHeightConstraint: NSLayoutConstraint!
+    
+    var titleText: String?
+    var messageText: String?
+    var numberOfButtons = 1
+    var buttonOneText: String?
+    var buttonTwoText: String?
+    var buttonOneAction: (()->Void)?
+    var buttonTwoAction: (()->Void)?
+    var alertWidth: CGFloat?
     
     let renderer = MongolUnicodeRenderer.sharedInstance
+    private let buttonTapHighlightColor = UIColor.lightGrayColor()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        alertTitle.text = renderer.unicodeToGlyphs(alertTitle.text)
-        alertMessage.text = renderer.unicodeToGlyphs(alertMessage.text)
         
-        alertContainerView.layer.cornerRadius = 8
+        // all alerts
+        view.backgroundColor = UIColor(white: 0, alpha: 0.4)
+        widthConstraint.constant = alertWidth ?? 200
+        alertContainerView.layer.cornerRadius = 13
+        alertContainerView.clipsToBounds = true
         
-        // Gesture recognizer to dismiss view
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissMongolAlert))
-        tapGesture.delegate = self
-        view.addGestureRecognizer(tapGesture)
+        if let unwrappedTitle = titleText {
+            alertTitle.text = renderer.unicodeToGlyphs(unwrappedTitle)
+        }
+        if let unwrappedMessage = messageText {
+            alertMessage.text = renderer.unicodeToGlyphs(unwrappedMessage)
+        }
+        
+        // No buttons
+        
+        if numberOfButtons == 0 {
+            
+            // tap to dismiss
+            let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissMongolAlert))
+            tapGesture.delegate = self
+            view.addGestureRecognizer(tapGesture)
+            
+            // hide buttons
+            buttonContainerWidthConstraint.constant = 0
+            horizontalDividerWidthConstraint.constant = 0            
+            
+            return
+        }
+        
+        // top button
+        
+        if let buttonOneText = buttonOneText {
+            topButton.text = renderer.unicodeToGlyphs(buttonOneText)
+        }
+        
+        let tapOne = UILongPressGestureRecognizer(target: self, action: #selector(buttonOneHandler))
+        tapOne.minimumPressDuration = 0
+        topButton.addGestureRecognizer(tapOne)
+        
+        if numberOfButtons == 1 {
+            
+            verticalDividerHeightConstraint.constant = 0
+            buttonEqualHeightsConstraint.active = false
+            bottomButtonHeightConstraint.constant = 0
+            
+            return
+        }
+        
+        // bottom button
+        
+        if let buttonTwoText = buttonTwoText {
+            bottomButton.text = renderer.unicodeToGlyphs(buttonTwoText)
+        }
+        
+        let tapTwo = UILongPressGestureRecognizer(target: self, action: #selector(buttonTwoHandler))
+        tapTwo.minimumPressDuration = 0
+        bottomButton.addGestureRecognizer(tapTwo)
         
     }
-
+    
+    func buttonOneHandler(gesture: UITapGestureRecognizer) {
+        
+        
+        if gesture.state == .Began{
+            topButton.backgroundColor = buttonTapHighlightColor
+        } else if gesture.state == UIGestureRecognizerState.Changed {
+            
+            // cancel gesture
+            gesture.enabled = false
+            gesture.enabled = true
+            topButton.backgroundColor = UIColor.clearColor()
+            
+        } else if  gesture.state == .Ended {
+            topButton.backgroundColor = UIColor.clearColor()
+            if let action = self.buttonOneAction {
+                action()
+            }
+            
+            dismissMongolAlert()
+        }
+    }
+    
+    func buttonTwoHandler(gesture: UITapGestureRecognizer) {
+        
+        if gesture.state == .Began {
+            bottomButton.backgroundColor = buttonTapHighlightColor
+        } else if gesture.state == UIGestureRecognizerState.Changed {
+            
+            // cancel gesture
+            gesture.enabled = false
+            gesture.enabled = true
+            bottomButton.backgroundColor = UIColor.clearColor()
+            
+        } else if  gesture.state == .Ended {
+            bottomButton.backgroundColor = UIColor.clearColor()
+            if let action = self.buttonTwoAction {
+                action()
+            }
+            dismissMongolAlert()
+        }
+        
+        
+        
+    }
+    
     func dismissMongolAlert() {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
+    
+
 }
